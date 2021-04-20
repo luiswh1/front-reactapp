92db5c0bfd590382c91b3b28b795dea55c9accb5
=Luis <=luisw.h@live.com>
2021-04-20 18:34:09 -0300

    css changes

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -33,31 +33,7 @@ const useStyles = makeStyles((theme) => ({
     heroContent: {
         padding: theme.spacing(8, 0, 6),
     },
-
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-    },
-    root2: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-    footer: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(6),
-    },
+ 
 }));
 
 export default function MainAvatar() {
@@ -65,20 +41,19 @@ export default function MainAvatar() {
 
     return(
         <main>
-                {/* Hero unit */}
                 <div className={classes.heroContent}>
                     <Container maxWidth="sm">
-                        <Typography className={classes.nome} component="h1" variant="h3" color="textPrimary" gutterBottom>
+                           <Typography className={classes.nome} component="h1" variant="h3" color="textPrimary" gutterBottom>
                             Luis Weber
-            </Typography>
-                        <img className={classes.image} alt="" src="https://scontent.fpet4-1.fna.fbcdn.net/v/t1.6435-9/43086294_2183954985225980_5066821374912233472_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=174925&_nc_ohc=27P_1_3LnSAAX9Hi5az&_nc_ht=scontent.fpet4-1.fna&oh=3df1a828b9945aa6f0044d3c82ba0529&oe=60A1C794"
+                         </Typography>  
+                            <img className={classes.image} alt="" src="https://scontent.fpet4-1.fna.fbcdn.net/v/t1.6435-9/43086294_2183954985225980_5066821374912233472_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=174925&_nc_ohc=27P_1_3LnSAAX9Hi5az&_nc_ht=scontent.fpet4-1.fna&oh=3df1a828b9945aa6f0044d3c82ba0529&oe=60A1C794"
                             width="50%"
                             height="50%">
-                        </img>
+                            </img>
                         <Typography className={classes.descriptionAndButtons} variant="h5" align="center" color="textSecondary" paragraph>
                         Extrovertido e comunicativo, busco cada dia mais me adaptar e a aprender as tecnologias mais utilizadas no mercado. <br />
-Meu foco em resolver problemas é absoluto e eu só paro com a solução em mãos. 
-            </Typography>
+                        Meu foco em resolver problemas é absoluto e eu só paro com a solução em mãos. 
+                         </Typography>
                         <div className={classes.heroButtons}>
                             <Grid container spacing={2} justify="center">
                                 <Grid item>
